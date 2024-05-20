f70cd8f9c49dd06342b0ce9dea5aa5fadad0db20
Johan Melin <soldierheart@hotmail.com>
2024-05-20 23:17:48 +0200

    remove app-example, keep +html and +not-found

--- app/+html.tsx
+++ app/+html.tsx
@@ -12,7 +12,10 @@ export default function Root({ children }: PropsWithChildren) {
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-
+        {/*
+          <meta name="description" content="This is a description of my web app." />
+          <meta name="keywords" content="web, app, react, native" />
+        */}
         {/*
           Disable body scrolling on web. This makes ScrollView components work closer to how they do on native.
           However, body scrolling is often nice to have for mobile web. If you want to enable it, remove this line.
