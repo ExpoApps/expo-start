c61920b43ba09b65718bfd26c0ad7f9cbf375e77
Johan Melin <soldierheart@hotmail.com>
2024-05-22 19:31:26 +0200

    make Header component

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,6 @@
-import { Stack, Link } from "expo-router";
-import { View, Pressable } from 'react-native';
-import { NavBarLink } from "@/components/navigation/NavBarLink";
-import { isDesktop } from "@/utils/screenUtils";
-import { UserSettingsProvider, useUserSettings } from "@/context/UserSettingsContext";
-import { ThemedIcon, ThemedText, ThemedView } from '@/components';
-import { useThemeColor } from '@/hooks/useThemeColor';
+import { Stack } from "expo-router";
+import { UserSettingsProvider } from "@/context/UserSettingsContext";
+import Header from "@/components/navigation/Header";
 
 export default function RootLayout() {
   return (
@@ -18,36 +14,3 @@ export default function RootLayout() {
   );
 }
 
-const Header = () => {
-  const userSettings = useUserSettings();
-  const showText = isDesktop();
-
-  return (
-    <ThemedView style={{
-      flexDirection: "row", 
-      justifyContent: "space-between", 
-      padding: 5,
-      borderBottomWidth: 1,
-      borderBottomColor: useThemeColor({}, 'gray'),
-    }}>
-    <ThemedText type="subtitle">
-      App
-    </ThemedText>
-    <View style={{flexDirection: "row", alignItems: "center"}}>
-      <Link href="/">
-        {showText
-        ? <NavBarLink name="Schedule" />
-        : <ThemedIcon name="calendar" size={25} style={{marginHorizontal: 5}} />}
-      </Link>
-      <Link href="/page">
-        {showText
-        ? <NavBarLink name="Reports" />
-        : <ThemedIcon name="bar-chart-2" size={25} style={{marginHorizontal: 5}} />}
-      </Link>
-      <Pressable onPress={userSettings.toggleTheme}>
-        <ThemedIcon name={userSettings.theme === "dark" ? "sun" : "moon"}size={25} style={{marginHorizontal: 5}} />
-      </Pressable>
-    </View>
-  </ThemedView>
-  );
-}
---
+++ components/navigation/Header.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import {  Link } from "expo-router";
+import { View, Pressable } from 'react-native';
+import { NavBarLink } from "@/components/navigation/NavBarLink";
+import { isDesktop } from "@/utils/screenUtils";
+import { useUserSettings } from "@/context/UserSettingsContext";
+import { ThemedIcon, ThemedText, ThemedView } from '@/components';
+import { useThemeColor } from '@/hooks/useThemeColor';
+
+const Header = () => {
+    const userSettings = useUserSettings();
+    const showText = isDesktop();
+  
+    return (
+      <ThemedView style={{
+        flexDirection: "row", 
+        justifyContent: "space-between", 
+        padding: 5,
+        borderBottomWidth: 1,
+        borderBottomColor: useThemeColor({}, 'gray'),
+      }}>
+      <ThemedText type="subtitle">
+        App
+      </ThemedText>
+      <View style={{flexDirection: "row", alignItems: "center"}}>
+        <Link href="/">
+          {showText
+          ? <NavBarLink name="Schedule" />
+          : <ThemedIcon name="calendar" size={25} style={{marginHorizontal: 5}} />}
+        </Link>
+        <Link href="/page">
+          {showText
+          ? <NavBarLink name="Reports" />
+          : <ThemedIcon name="bar-chart-2" size={25} style={{marginHorizontal: 5}} />}
+        </Link>
+        <Pressable onPress={userSettings.toggleTheme}>
+          <ThemedIcon name={userSettings.theme === "dark" ? "sun" : "moon"}size={25} style={{marginHorizontal: 5}} />
+        </Pressable>
+      </View>
+    </ThemedView>
+    );
+  }
+
+export default Header
