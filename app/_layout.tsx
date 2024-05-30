9cfa68d3cc728aac3db942aafdcfd4c91a47b284
Johan Melin <soldierheart@hotmail.com>
2024-05-30 12:21:02 +0200

    reintroduce header. add google verify

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,11 @@
 import { Stack } from "expo-router";
 import { UserSettingsProvider } from "@/context/UserSettingsContext";
+import Header from "@/components/navigation/Header";
 
 export default function RootLayout() {
   return (
     <UserSettingsProvider>
-      <Stack>
+      <Stack screenOptions={{header: () => <Header />}}>
         <Stack.Screen name="index" />
         <Stack.Screen name="page" />
         <Stack.Screen name="menu" />
